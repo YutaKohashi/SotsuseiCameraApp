d7ce795dde4894a129ddc250a26c455461e76590
XiaomiMiAir12\yuta <kaerukerokero1995@gmail.com>
2017-10-21 12:45:57 +0900

    トースト処理リソースを追加

--- app/src/main/java/jp/yuta/kohashi/sotsuseicameraapp/ui/ToastHelper.kt
+++ app/src/main/java/jp/yuta/kohashi/sotsuseicameraapp/ui/ToastHelper.kt
@@ -1,5 +1,6 @@
 package jp.yuta.kohashi.sotsuseicameraapp.ui
 
+import android.support.annotation.StringRes
 import android.widget.Toast
 import jp.yuta.kohashi.sotsuseicameraapp.App
 import jp.yuta.kohashi.sotsuseicameraapp.R
@@ -12,15 +13,21 @@ import jp.yuta.kohashi.sotsuseicameraapp.utils.ResUtil
  * Date : 11 / 10 / 2017
  */
 
-object ToastHelper {
+object  ToastHelper {
 
-    fun alreadyRunningService() = show(ResUtil.string(R.string.already_running_servce))
+    fun alreadyRunningService() = show(R.string.already_running_servce)
 
-    fun runService() = show(ResUtil.string(R.string.success_runnning_service))
+    fun runService() = show(R.string.success_runnning_service)
 
-    fun stopService() = show(ResUtil.string(R.string.success_stop_servivce))
+    fun stopService() = show(R.string.success_stop_servivce)
 
-    fun alreadyStopService() = show(ResUtil.string(R.string.already_stop_service))
+    fun alreadyStopService() = show(R.string.already_stop_service)
+
+    fun cameraAccessException() = show(R.string.camera_access_exception)
+
+    fun noPermission() = show(R.string.no_permission)
+
+    fun show(@StringRes id:Int) = show(ResUtil.string(id))
 
     fun show(text: String) {
         Toast.makeText(App.context, text, Toast.LENGTH_SHORT).show()
